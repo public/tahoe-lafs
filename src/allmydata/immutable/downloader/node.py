f7ccea542e320c2497699890fbb270ab0af6856e
Brian Warner <warner@lothar.com>
2011-09-30 02:00:25 +0100

    disable block_hash_tree and ciphertext_hash_tree fetch and check
    see how this affects the speed. Also disabled readv2 (since memoryview isn't
    available on my py2.6 laptop), so compare this against readv1.

--- src/allmydata/immutable/downloader/node.py
+++ src/allmydata/immutable/downloader/node.py
@@ -475,9 +475,9 @@ class DownloadNode:
         assert self.segment_size is not None
         offset = segnum * self.segment_size
 
-        h = hashutil.crypttext_segment_hash(segment)
+        #h = hashutil.crypttext_segment_hash(segment)
         try:
-            self.ciphertext_hash_tree.set_hashes(leaves={segnum: h})
+            #self.ciphertext_hash_tree.set_hashes(leaves={segnum: h})
             return (offset, segment, decodetime)
         except (BadHashError, NotEnoughHashesError):
             format = ("hash failure in ciphertext_hash_tree:"
