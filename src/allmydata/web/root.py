89a41005d64d95a0c602855561287152c33cbaab
Brian Warner <warner@lothar.com>
2011-10-11 19:13:33 -0700

    web: add private control panel, at /control/KEY/
    with a secret URL that can be generated from information in
    ~/.tahoe/private/control.key . General idea is to put node-local secrets and
    controls here: grid membership, checker/repairer management service controls,
    periodic local backup controls. This is like control.furl, but web-based.

--- src/allmydata/web/root.py
+++ src/allmydata/web/root.py
@@ -1,12 +1,14 @@
 import time
 
 from twisted.internet import address
-from twisted.web import http
+from twisted.web import http, static
+from twisted.web.resource import Resource
 from nevow import rend, url, loaders, tags as T
 from nevow.inevow import IRequest
 from nevow.static import File as nevow_File # TODO: merge with static.File?
 from nevow.util import resource_filename
 from formless import webform
+from hashlib import sha256
 
 import allmydata # to display import path
 from allmydata import get_package_versions_string
@@ -144,6 +146,38 @@ class NoReliability(rend.Page):
 
 SPACE = u"\u00A0"*2
 
+def sfile(name):
+    return static.File(resource_filename("allmydata.web", name))
+def contents(name):
+    return open(resource_filename("allmydata.web", name), "rb").read()
+
+class ControlPanel(Resource):
+    def __init__(self, client):
+        Resource.__init__(self)
+        self.client = client
+        # putChild("") makes /control/KEY/ work (as opposed to /control/KEY
+        # without the trailing slash), which lets us use relative links from
+        # this page
+        self.putChild("", self)
+
+    def render_GET(self, request):
+        return contents("control.html")
+
+class ControlPanelGuard(Resource):
+    def __init__(self, client):
+        Resource.__init__(self)
+        self.client = client
+
+    def render_GET(self, request):
+        return "I want a key. Look in ~/.tahoe/private/control.key\n"
+
+    def getChild(self, name, request):
+        key = self.client.get_control_url_key()
+        # constant-time comparison
+        if sha256(key).digest() != sha256(name).digest():
+            raise WebError("bad key in /control/KEY request\n")
+        return ControlPanel(self.client)
+
 class Root(rend.Page):
 
     addSlash = True
@@ -168,6 +202,7 @@ class Root(rend.Page):
         self.child_named = FileHandler(client)
         self.child_status = status.Status(client.get_history())
         self.child_statistics = status.Statistics(client.stats_provider)
+        self.child_control = ControlPanelGuard(client)
         def f(name):
             return nevow_File(resource_filename('allmydata.web', name))
         self.putChild("download_status_timeline.js", f("download_status_timeline.js"))
